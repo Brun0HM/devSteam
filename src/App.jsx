4490a469dff080163cc3284b7a657d64c5b04a5f
Brun0 <brunodev41@gmail.com>
2025-04-22 10:54:38 -0300

    Inserir os cards promoção

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import Promotion from "./components/Promotion";
 
 const App = () => {
   return (
     <>
       <Header />
+      <Promotion />
     </>
   );
 };
---
+++ src/components/Promotion.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const promotion = () => {
+  return (
+   <div id="promotion">
+    <h2>Promoções</h2>
+    <div id='ItensPromo'>
+
+    </div>
+   </div>
+  )
+}
+
+export default promotion
