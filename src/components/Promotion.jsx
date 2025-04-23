25e653b73bb0124ed3dba0a4d08a77407d1bba5c
Brun0 <brunodev41@gmail.com>
2025-04-23 14:48:06 -0300

    Adiciona ordenação aleatória de jogos promocionais na primeira renderização sem o sort

--- src/components/Promotion.jsx
+++ src/components/Promotion.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PromoCard from "./PromoCard";
 
 const Promotion = (props) => {
@@ -69,6 +69,16 @@ const Promotion = (props) => {
     },
   ];
 
+  const [orderedGames, setOrderedGames] = useState([]);
+
+  useEffect(() => {
+    // Ordena os jogos apenas na primeira renderização
+    const shuffledGames = games
+      .filter((jogo) => jogo.desconto > 0)
+      .sort(() => Math.random() - 0.5); // Ordenação aleatória
+    setOrderedGames(shuffledGames);
+  }, []);
+
   return (
     <div id="promotion" className="container w-75 my-4">
       <h2 className="text-uppercase text-center text-md-start ms-md-5 ps-md-3 mb-4">
@@ -78,24 +88,16 @@ const Promotion = (props) => {
         id="itensPromo"
         className="d-flex flex-wrap gap-4 justify-content-around"
       >
-        {/* mapeando um array com react */}
-        {games
-          .filter((jogo) => jogo.desconto > 0)
-          //.sort((a, b) => b.desconto - a.desconto) //ordenação por desconto decrescente
-          .sort(() => Math.random() - 0.5) //ordenação aleatória
-          .slice(0, 3)
-          .map((jogo) => (
-            <PromoCard
-              key={jogo.id}
-              titulo={jogo.titulo}
-              preco={jogo.preco.toFixed(2)}
-              desconto={jogo.desconto}
-              imagem={jogo.imagem}
-              //adicionando a opção de click com os itens do jogo no carrinho
-              onAddCarrinho={() => props.onAddCarrinho(jogo)}
-              //callback para adicionar somente um item e não todos os itens do array
-            />
-          ))}
+        {orderedGames.slice(0, 3).map((jogo) => (
+          <PromoCard
+            key={jogo.id}
+            titulo={jogo.titulo}
+            preco={jogo.preco.toFixed(2)}
+            desconto={jogo.desconto}
+            imagem={jogo.imagem}
+            onAddCarrinho={() => props.onAddCarrinho(jogo)}
+          />
+        ))}
       </div>
     </div>
   );
