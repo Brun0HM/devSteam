61e0ef10eb40fc268ac4659df1b404274a27b86c
Brun0 <brunodev41@gmail.com>
2025-04-23 07:35:51 -0300

    Adiciona funcionalidade de carrinho e contador no cabeçalho

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import "./App.css";
+import { useState } from "react";
 import Header from "./components/Header";
 import Promotion from "./components/Promotion";
 
 const App = () => {
+  const [carrinhoItem, setCarinhoItem] = useState([]);
+  const [mostraCarrinho, setMostraCarrinho] = useState(false);
+
+  const adicionarCarrinho = (produto) => {
+ setCarinhoItem((produtosAnteriores)=>{
+  const existing = produtosAnteriores.find((item) => item.id === produto.id);
+  if 
+ })
+  }
+
+
   return (
     <>
-      <Header />
-      <Promotion />
+      <Header contadorJogos={2} />
+      <Promotion  adicionarCarrinho={adicionarCarrinho}/>
     </>
   );
 };
