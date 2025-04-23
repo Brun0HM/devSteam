42bbfabd1d5581246ab69dacba8b2ac6adfb4d33
Brun0 <brunodev41@gmail.com>
2025-04-23 08:04:34 -0300

    entendendo processo de adicionar item no header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import "./App.css";
 import Header from "./components/Header";
 import Promotion from "./components/Promotion";
+import { useState } from "react";
 
 function App() {
+  const [contador, setContador] = useState(0);
+
+  const handleAddToCart = () => {
+    setContador(contador + 1);
+  };
+
   return (
     <>
-      <Header contadorJogos={2} />
+      <Header contadorJogos={contador} />
       <Promotion />
+
+      <button onClick={handleAddToCart}>bla</button>
     </>
   );
 }
